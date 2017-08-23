596837f51861e6c91c3270652bfb87f841c223d2
edrury <621601+edrury@users.noreply.github.com>
2017-08-22 23:47:46 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,5 +4,20 @@
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
-   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+  wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+
+add_action( 'init', 'remove_parent_action');
+
+function remove_parent_action() {
+  remove_action( 'oblique_footer', 'oblique_footer_credits' );
+}
+
+/**
+ * Footer credits
+ */
+add_action( 'oblique_footer', 'oblique_child_footer_credits' );
+
+function oblique_child_footer_credits() {
+  echo 'Copyright © 2017 SB23 Productions.';
 }
